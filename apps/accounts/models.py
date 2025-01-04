7429453d42e9a1336165c10ba969f686af3d1247
Tolu Joel <toluisjoel@gmail.com>
2025-01-04 22:19:44 +0100

    update(accounts): Add `account_balance` and `next_cursor` fields to profile model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     plaid_access_token = models.CharField(max_length=255, blank=True, null=True)
+    account_balance = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
+
+    next_cursor = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
-
